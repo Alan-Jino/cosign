abfd73b7bf1a760ae706f1108896cd5977565dad
Alan-Jino <jinolavi33@gmail.com>
2023-07-12 11:49:16 +0530

    cosign testing

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+func main() {
+
+	fmt.Println("HELLO WORLD")
+
+	num := 5
+	result := factorial(num)
+	fmt.Printf("The factorial of %d is %d\n", num, result)
+
+}
